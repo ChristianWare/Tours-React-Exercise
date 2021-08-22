66dfba5d74323dbdb17a0fa92a1eb5e4dd695c94
Chris <chris.ware.dev@gmail.com>
2021-08-21 22:46:07 -0700

    completed

--- src/Tours.js
+++ src/Tours.js
@@ -1,11 +1,18 @@
 import React from "react";
 import Tour from "./Tour";
-
-const Tours = () => {
+const Tours = ({ tours, removeTour }) => {
   return (
-    <div>
-      <h2>Tours Component</h2>
-    </div>
+    <section>
+      <div className='title'>
+        <h2>our tours</h2>
+        <div className='underline'></div>
+      </div>
+      <div>
+        {tours.map((tour) => {
+          return <Tour key={tour.id} {...tour} removeTour={removeTour} />;
+        })}
+      </div>
+    </section>
   );
 };
 
